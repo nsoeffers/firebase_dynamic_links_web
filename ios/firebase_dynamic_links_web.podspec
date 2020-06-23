c97f86d7c9a4c322f9cfae919e4507ae4d8ff7dd
niels.soeffers <n.soeffers@aca-it.be>
2020-06-23 22:06:48 +0200

    Initial commit

---
+++ ios/firebase_dynamic_links_web.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+    s.name             = 'firebase_dynamic_links_web'
+    s.version          = '0.0.1'
+    s.summary          = 'No-op implementation of firebase_dynamic_links_web web plugin to avoid build issues on iOS'
+    s.description      = <<-DESC
+  temp fake firebase_dynamic_links_web plugin
+                         DESC
+    s.homepage         = ''
+    s.license          = { :file => '../LICENSE' }
+    s.author           = { 'Niels Soeffers' => 'nsoeffers@gmail.com' }
+    s.source           = { :path => '.' }
+    s.source_files = 'Classes/**/*'
+    s.public_header_files = 'Classes/**/*.h'
+    s.dependency 'Flutter'
+
+    s.ios.deployment_target = '8.0'
+  end
---
+++ lib/firebase_dynamic_links_web.dart
@@ -0,0 +1,65 @@
+library firebase_dynamic_links_web;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:js';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:http/http.dart' as http;
+
+/// A Calculator.
+class FirebaseDynamicLinksWebPlugin {
+
+    static void registerWith(Registrar registrar) {
+        final MethodChannel channel = MethodChannel(
+            'plugins.flutter.io/firebase_dynamic_links',
+            const StandardMethodCodec(),
+            registrar.messenger
+        );
+        final FirebaseDynamicLinksWebPlugin instance = FirebaseDynamicLinksWebPlugin();
+        channel.setMethodCallHandler(instance.handleMethodCall);
+    }
+
+
+    Future<dynamic> handleMethodCall(MethodCall call) async {
+        switch (call.method) {
+            case 'DynamicLinkParameters#buildUrl':
+                return _buildUrl(call.arguments);
+            case 'DynamicLinkParameters#shortenUrl':
+                return _shortenUrl(call.arguments);
+            case 'FirebaseDynamicLinks#getInitialLink':
+                return _getInitialLink();
+            default:
+                throw PlatformException(
+                    code: 'Unimplemented',
+                    details: "The firebase_dynamic_links plugin for web doesn't implement "
+                        "the method '${call.method}'");
+        }
+    }
+
+    _getInitialLink() {
+        return Completer().future;
+    }
+
+    _buildUrl(params) {
+        return "${params["uriPrefix"]}?link=${params["link"]}&apn=${params["androidParameters"]["packageName"]}";
+    }
+
+    _shortenUrl(params) async {
+        final apiKey = 'AIzaSyD_PbghcLOA1w1zj5UZtBqsc9FdjBY1DYQ';
+        final url = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${apiKey}';
+        final body = {
+            "longDynamicLink": params["url"],
+            if (params["dynamicLinkParametersOptions"] != null ) "suffix": {
+                "option": params["dynamicLinkParametersOptions"]["shortDynamicLinkPathLength"] == 0 ? "UNGUESSABLE" : "SHORT"
+            }
+        };
+        final response = await http.post(url, body: json.encode(body));
+        final shortLinkInfo = json.decode(response.body);
+        return {
+            "url": shortLinkInfo["shortLink"]
+        };
+
+    }
+}
